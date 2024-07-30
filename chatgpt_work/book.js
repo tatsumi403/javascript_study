82148b3bf2f2597f27e50cbe0b2949b3328976e4
tatsumi403 <greenalpina.b5@gmail.com>
2024-07-30 23:39:20 +0900

    compareAgeメソッド修正、getSummaryメソッド作成

--- chatgpt_work/book.js
+++ chatgpt_work/book.js
@@ -8,7 +8,7 @@ class Book {
   // メソッド
   // 戻り値は文字列で、形式は｀タイトル by 著者、出版年｀とする
   getSummary(title, author, year) {
-    let summary = title + " by " + author + ", " + year;
+    let summary = title + " by " + author + ", " + year + "年";
     return summary;
   }
   // メソッド
@@ -17,11 +17,17 @@ class Book {
   // getAage() {
   //   return;
   // }
+
+  // メソッド
+  compareAge(book1, book2) {
+    let more_old;
+    return more_old;
+  }
 }
 
 let book1 = new Book();
 let title = "世界一流エンジニアの思考法";
 let author = "牛尾剛";
 let year = "2023";
-let aaa = book1.getSummary(title, author, year);
-console.log(aaa);
+let ans = book1.getSummary(title, author, year);
+console.log(ans);
