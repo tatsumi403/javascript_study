d928ec6d468104f6dbf167c86ba31baee0f7c1b5
tatsumi403 <greenalpina.b5@gmail.com>
2024-08-17 10:59:21 +0900

    自力で実装

---
+++ chatgpt_work/pet_class/pet_class.js
@@ -0,0 +1,39 @@
+class Pet {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+        this.isHungry = true;
+    }
+
+    // メソッド
+    // ペットに餌を与えてisHungryプロパティをfalseに変えてメッセージを出力
+    feed() {
+        this.isHungry = false;
+        console.log(`${this.name} is no longer hungry.`);
+    }
+
+    // ペットと遊ぶ
+    play() {
+        if (this.isHungry) {
+            console.log(`${this.name} is too hungry to play.`);
+        }
+        console.log(`${this.name} is playing happily.`);
+    }
+
+    // ペットと話す
+    speak() {
+        if (this.type === 'dog') {
+            console.log('Woof!');
+        } else if (this.type === 'cat') {
+            console.log('Meow!');
+        } else {
+            console.log('Hmm!');
+        }
+    }
+}
+
+const myPet = new Pet('Moco', 'dog');
+myPet.play();
+myPet.feed();
+myPet.play();
+myPet.speak();
