14e84d8898a6ef53ba8135690518f1b2a4efa7b9
tatsumi403 <greenalpina.b5@gmail.com>
2024-09-01 23:55:56 +0900

    本の追加/貸し出し/返却メソッド実装

---
+++ chatgpt_work/library_class/library.js
@@ -0,0 +1,67 @@
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  //
+  addBook(title, author) {
+    this.books.title = title;
+    this.books.author = author;
+    this.books.available = available;
+  }
+
+  //
+  borrowBook(title) {
+    if (this.books.title == title) {
+      if (this.books.available == true) {
+        this.books.available = false;
+        return "貸し出し成功!";
+      } else {
+        return "エラー!その本は貸し出しできません!";
+      }
+    } else {
+      return "エラー!その本は貸し出しできません!";
+    }
+  }
+
+  // 本を返却するメソッド
+  returnBook(title) {
+    if (this.books.title == title) {
+      if (this.books.available == false) {
+        this.books.available = true;
+        return "返却成功!";
+      } else {
+        return "エラー!その本は返却できません!";
+      }
+    } else {
+      return "エラー!その本は返却できません";
+    }
+  }
+
+  // 作成中
+  // 本が貸し出し可能か確認するメソッド
+  isAvailable(title) {
+    if (this.books.available) {
+      return;
+    }
+  }
+}
+
+// Library クラスのインスタンスを作成
+let library = new Library();
+let available = true;
+// 本を追加
+library.addBook("The Great Gatsby", "F. Scott Fitzgerald");
+library.addBook("1984", "George Orwell");
+
+// 本の貸し出し
+console.log(library.borrowBook("1984")); // 貸し出し成功メッセージ
+// console.log(library.isAvailable("1984")); // false
+
+// 本の返却
+console.log(library.returnBook("1984")); // 返却成功メッセージ
+// console.log(library.isAvailable("1984")); // true
+
+// デバッグ
+// console.log(library.borrowBook("1984"));
+// console.log(library.books);
