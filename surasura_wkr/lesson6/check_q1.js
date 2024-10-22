7ef5ba1c8da0526760984c624da3a1f07fe004ac
tatsumi403 <greenalpina.b5@gmail.com>
2024-10-22 23:37:44 +0900

    if文 三項演算子練習

---
+++ surasura_wkr/lesson6/check_q1.js
@@ -0,0 +1,15 @@
+// const a = 5;
+// const b = 3;
+// let c;
+// if (a <= b) {
+//   c = a;
+// } else {
+//   c = b;
+// }
+// console.log(c);
+
+const a = 5;
+const b = 3;
+let c;
+const result = a <= b ? (c = a) : (c = b);
+console.log(result);
